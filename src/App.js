94f25cb6a4f6e71d9a68dc84632a9918b882ff26
Carl Tablante <carl@guavapass.com>
2017-06-02 08:38:53 +0800

    Semantic ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'semantic-ui/dist/semantic.css';
 import './App.css';
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
+    const imgPath = "./images/mactan.png";
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="red ui inverted menu">
+          <a className="item">
+            <img src={logo} />
+            Lagalag
+          </a>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="ui card">
+          <div className="content">
+            <div className="right floated meta">14h</div>
+            <img className="ui avatar image" src="./images/mactan.png"/> Elliot
+          </div>
+          <div className="image">
+            <img src="images/mactan.png"/>
+          </div>
+          <div className="content">
+            <span className="right floated">
+              <i className="heart outline like icon"></i>
+              17 likes
+            </span>
+            <i className="comment icon"></i>
+            3 comments
+          </div>
+          <div className="extra content">
+            <div className="ui large transparent left icon input">
+              <i className="heart outline icon"></i>
+              <input type="text" placeholder="Add Comment..."/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
